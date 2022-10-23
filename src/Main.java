68eb0a5074b5267752e4124a3342323a930fe7b6
Lubava <Lwm28@rambler.ru>
2022-10-23 13:29:27 +0300

    дз3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+       public static void main(String[] args) {
+        int beg =27897;
+        System.out.println(beg);
+        byte dog =67;
+System.out.println(dog);
+         short hous=-159;
+         System.out.println(hous);
+         long cat=987678965549;
+         System.out.print(cat);
+         doubl bigHous=5.786;
+         System.out.println(bigHous);
+         fioat wings=27.12;
+         System.out.println(wings);
+         cher bar=43;
+         System.out.println(bar);
+         boolean b = Float;
+         System.out.println(b);
+
     }
 }
